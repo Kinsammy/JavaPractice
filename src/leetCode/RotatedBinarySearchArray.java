7975e2f6fa4dc5628f4f3244baa96a780a3807f1
Kinsammy <fanusamuel@gmail.com>
2024-01-09 12:10:46 +0100

    BinarySearch with test cases

--- src/leetCode/RotatedBinarySearchArray.java
+++ src/leetCode/RotatedBinarySearchArray.java
@@ -4,6 +4,37 @@ public class RotatedBinarySearchArray {
     public static void main(String[] args) {
 
     }
+    
+    
+    public static int findTargetIndex(int[] arr, int target) {
+        int pivot = findPivot(arr);
+//        if no pivot is found, apply binary search
+        if (pivot == -1) {
+            return binarySearch(arr, target, 0, arr.length-1);
+        }
+        if (target >= arr[0]) {
+            return binarySearch(arr, target, 0, pivot-1);
+        }
+
+        return binarySearch(arr, target, pivot + 1, arr.length-1);
+    }
+
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target == arr[mid]) {
+                return mid;
+            }
+            if (target > arr[mid]) {
+                start = mid +1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return - 1;
+    }
+
 
     public static int findPivot(int[] arr){
         int start = 0;
--- test/leetCode/RotatedBinarySearchArrayTest.java
+++ test/leetCode/RotatedBinarySearchArrayTest.java
@@ -2,7 +2,7 @@ package leetCode;
 
 import org.junit.jupiter.api.Test;
 
-import static leetCode.RotatedBinarySearchArray.findPivot;
+import static leetCode.RotatedBinarySearchArray.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RotatedBinarySearchArrayTest {
@@ -12,5 +12,21 @@ class RotatedBinarySearchArrayTest {
         int[] arr = {4, 5, 6, 7, 0, 1, 2};
         int pivot = findPivot(arr);
         assertEquals(3, pivot);
+    }
+
+    @Test
+    void binarySearchTest() {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 6;
+        int ans = binarySearch(arr, target, 0, arr.length-1);
+        assertEquals(2, ans);
+    }
+
+    @Test
+    void findTargetIndexTest() {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 4;
+        int ans = findTargetIndex(arr, target);
+        assertEquals(0, ans);
     }
 }
